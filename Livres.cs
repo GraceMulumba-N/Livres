35b63593111254bb69aad07b8862df2dfa992d6b
Grace_Mulumba <grace.mulumbandaya@monboreal.ca>
2024-11-16 20:37:40 -0500

    Initialisation

---
+++ Livres.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Livres
+{
+    public class Livres
+    {
+        public string Titre { get; set; }
+        public string Auteur { get; set; }
+        public int NombrePages { get; set; }
+
+        public Livres(string titre, string auteur, int nbrPages) 
+        {
+            Titre = titre;
+            Auteur = auteur;
+            NombrePages = nbrPages;
+        }
+        public void AfficherDetails() 
+        {
+            Console.WriteLine($"Titre : {Titre}, Auteur : {Auteur}, Nombre de pages : {NombrePages}");
+        }
+    }
+}
