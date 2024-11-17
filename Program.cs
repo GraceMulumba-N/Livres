c869cc7485b949a9fbaabb72180a9fc99e5b2876
Grace_Mulumba <grace.mulumbandaya@monboreal.ca>
2024-11-16 21:48:10 -0500

    creation de la classe revue et de ses instances

--- Program.cs
+++ Program.cs
@@ -1,7 +1,17 @@
-﻿internal class Program
+﻿using Livres;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("Bienvenue dans Gestion de Livres!");
+
+        Revue revue1 = new Revue("The New Yorker", "Helen Shaw", 2, 234, 2022);
+        Revue revue2 = new Revue("The New York Times", "Michael Schulman", 3, 222, 1989);
+        Revue revue3 = new Revue("Roman 20-50", "Anne Wattel", 1, 76, 2023);
+
+        revue1.AfficherDetails();
+        revue2.AfficherDetails();
+        revue3.AfficherDetails();
     }
 }
--- Revue.cs
+++ Revue.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace Livres
 {
-    internal class Revue
+    public class Revue : Livres
     {
+        public int Numero { get; set; }
+        public int Annee { get; set; }
+        public Revue(string titre, string auteur, int nbrPages, int numero, int annee):
+            base(titre, auteur, nbrPages)
+        {
+            Numero = numero;
+            Annee = annee;
+        }
+        
+        public new void AfficherDetails()
+        {
+            Console.WriteLine($"Titre : {Titre}, Auteur : {Auteur}, Nombre de pages : {NombrePages}, Numero : {Numero}, Annee : {Annee}");
+        }
+
     }
 }
